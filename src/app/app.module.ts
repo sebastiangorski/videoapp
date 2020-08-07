31ef17679382ed8f58e799a6d09f23b2c872eafc
Sebastian Górski <silent8668@gmail.com>
2020-08-07 15:18:44 +0200

    Add common module incld. header and footer, add videos module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { AppCommonModule } from './modules/common/common.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CommonModule,
+    AppCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
